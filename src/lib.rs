9dcd562730a43b12b425dbded5fcf45dc72bf49c
Justin <justin.whitaker@usu.edu>
2024-01-31 00:42:11 -0700

    adjusting public api

--- src/lib.rs
+++ src/lib.rs
@@ -8,9 +8,33 @@
 //! with other modules supporting this primary module. However, the
 //! most commonly used functionality is re-exported to the top level
 //! for ease-of-use.
+//!
+//! Example:
+//! ```
+//! use dc::integrator::Integrator;
+//! use distributed_control as dc;
+//! use ndarray::{array, Array1};
+//!
+//! let lin_dyn = dc::LtiDynamics::new(array![[0.]], array![[1.]]);
+//! let mas = dc::HomMas::new(&lin_dyn, 3);
+//! let initial_states = array![-1., 0., 1.];
+//! let laplacian = array![[1., -1., 0.], [-1., 2., -1.], [0., -1., 1.]];
+//! let u = |_t: f64, x: &Array1<f64>| -laplacian.dot(x);
+//!
+//! let states = dc::EulerIntegration::simulate(
+//!     (0..100).map(|i| i as f64 * 0.1).collect(),
+//!     &initial_states,
+//!     &mas,
+//!     &u,
+//! );
+//! println!("{states}");
+//! println!("{}", states.column(states.ncols() - 1));
+//! ```
 
 pub mod mas;
 pub mod graphs;
 pub mod dynamics;
 pub mod integrator;
 pub use mas::*;
+pub use dynamics::LtiDynamics;
+pub use integrator::EulerIntegration;
