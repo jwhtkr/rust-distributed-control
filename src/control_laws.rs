39c15e2b1e333e0abf5a7104f8748336d1db4bf4
Justin <justin.whitaker@usu.edu>
2024-02-21 16:06:50 -0700

    added forced single-integrator consensus

--- src/control_laws.rs
+++ src/control_laws.rs
@@ -3,12 +3,12 @@
 
 use std::ops::Mul;
 
-use ndarray::{linalg::kron, Array1, Array2, LinalgScalar, ScalarOperand};
+use ndarray::{linalg::kron, s, Array1, Array2, LinalgScalar, ScalarOperand};
 use ndarray_linalg::{error::LinalgError, Lapack, SVD};
 
 /// Create the consensus feedback control law for homogenous single-integrators.
 ///
-/// Formed from the negative of the graph laplacian and the number of states of
+/// Formed from the negative of the graph Laplacian and the number of states of
 /// each agent. This is equivalent to
 /// $$
 ///     u(t, x) = -(L \otimes I_{n}) x
@@ -36,6 +36,37 @@ pub fn single_integrator_consensus<T: LinalgScalar>(
 ) -> impl Fn(T, &Array1<T>) -> Array1<T> {
     let feedback_mat = kron(&neg_laplacian, &Array2::eye(n_states));
     move |_t: T, x: &Array1<T>| -> Array1<T> { feedback_mat.dot(x) }
+}
+
+/// Create the control for single integrator forced consensus.
+///
+/// Formed from the negative of the communication graph Laplacian matrix, $L$,
+/// the pinning gains (from the augmented "leader-follower" graph), $k$, and the
+/// reference signal, $r$.
+/// Equivalent to:
+/// $$
+///     u(t) = -\left( (L + \operatorname{diag}(k)) \otimes I_n \right)x + k \otimes r
+/// $$
+/// where $\otimes$ denotes that Kronecker product and $n$ is the size of the
+/// state.
+pub fn single_integrator_forced_consensus<T: LinalgScalar>(
+    neg_laplacian: &Array2<T>,
+    pinning_gains: &Array1<T>,
+    reference: impl Fn(T, &Array1<T>) -> Array1<T>,
+    n_states: usize,
+) -> impl Fn(T, &Array1<T>) -> Array1<T> {
+    let pinning_gains = pinning_gains.clone();
+    let neg_l_plus_k = neg_laplacian - Array2::from_diag(&pinning_gains);
+    let state_feedback = kron(&neg_l_plus_k, &Array2::eye(n_states));
+    move |t: T, x: &Array1<T>| -> Array1<T> {
+        let r_vec = reference(t, x);
+        let pinned_ref = kron(
+            &pinning_gains.slice(s![.., ndarray::NewAxis]),
+            &r_vec.insert_axis(ndarray::Axis(1)),
+        )
+        .remove_axis(ndarray::Axis(1));
+        state_feedback.dot(x) + pinned_ref
+    }
 }
 
 /// Create the leaderless synchronizing controller for a homogenous MAS
@@ -67,7 +98,10 @@ pub fn coupling_gain<T: LinalgScalar + Lapack<Real = T> + std::cmp::PartialOrd>(
     laplacian: &Array2<T>,
 ) -> Result<T, LinalgError> {
     let (_, eig, _) = SVD::svd(laplacian, false, false)?;
-    let nonzero = eig.iter().map(|&v| v.re()).filter(|&v| T::from_f64(1e-12).unwrap() < v);
+    let nonzero = eig
+        .iter()
+        .map(|&v| v.re())
+        .filter(|&v| T::from_f64(1e-12).unwrap() < v);
     let min_nonzero = nonzero
         .reduce(|v1, v2| if v1 <= v2 { v1 } else { v2 })
         .unwrap();
@@ -119,12 +153,17 @@ mod tests {
                 Default::default(),
                 Default::default(),
                 Default::default(),
-            ).unwrap(),
-        ).unwrap();
+            )
+            .unwrap(),
+        )
+        .unwrap();
         let c = coupling_gain(&laplacian).unwrap();
         let control = homogenous_leaderless_synchronization(&-laplacian, c, &k_mat);
         let state_step = EulerIntegration::step(0.0, 1.0, &x0, &mas, &control);
 
-        assert!(state_step.abs_diff_eq(&array![-1., 0.5, 0., 2.232050807568878, 4., -0.732050807568878], 1e-7))
+        assert!(state_step.abs_diff_eq(
+            &array![-1., 0.5, 0., 2.232050807568878, 4., -0.732050807568878],
+            1e-7
+        ))
     }
 }
